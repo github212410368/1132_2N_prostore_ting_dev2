afd2f8d661f2d2ac82819efd674a2237b1f2ed02
ting <212410368@o365.tku.edu.tw>
2025-06-20 03:03:02 +0800

    test2

--- components/cabins/Navigation_xx.tsx
+++ components/cabins/Navigation_xx.tsx
@@ -6,16 +6,19 @@ import React from 'react';
 
 const links = [
   {
+    id: 'cabins',
     title: 'Cabins_xx',
     href: '/cabins',
   },
   {
+    id: 'about',
     title: 'About_xx',
-    href: '/cabins',
+    href: '/about', // 修正 href 為獨立路徑
   },
   {
+    id: 'guest',
     title: 'GuestArea_xx',
-    href: '/cabins',
+    href: '/guest-area', // 修正 href 為獨立路徑
   },
 ];
 
@@ -24,18 +27,16 @@ const Navigation_xx = ({
   ...props
 }: React.HTMLAttributes<HTMLElement>) => {
   const pathname = usePathname();
+
   return (
-    <nav
-      className={cn('flex items-center space-x-4 lg:space-x-6', className)}
-      {...props}
-    >
+    <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)} {...props}>
       {links.map((item) => (
         <Link
-          key={item.href}
+          key={item.id} // 避免使用 href 作為 key
           href={item.href}
           className={cn(
             'text-sm font-medium transition-colors hover:text-primary',
-            pathname.includes(item.href) ? '' : 'text-muted-foreground'
+            pathname === item.href ? 'text-primary' : 'text-muted-foreground'
           )}
         >
           {item.title}
