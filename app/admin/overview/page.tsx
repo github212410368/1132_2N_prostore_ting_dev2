243fdae5a245cc42f6fe5e47cb03ab51cc4370b4
ting <212410368@o365.tku.edu.tw>
2025-06-20 03:33:49 +0800

    test4

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,8 +1,12 @@
 import { requireAdmin } from '@/lib/auth.guard';
 
-const OverviewPage_xx = async () => {
-  await requireAdmin();
-
+const OverviewPage_xx = () => {
   return <div>OverviewPage_xx</div>;
 };
+
+export async function getServerSideProps() {
+  await requireAdmin();
+  return { props: {} };
+}
+
 export default OverviewPage_xx;
